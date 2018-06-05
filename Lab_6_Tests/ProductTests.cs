5cc3bdecb87ee9c5c381d00c4213db9aac67b2af
Daniel Coats <coatsd@my.lanecc.edu>
2018-06-05 15:58:45 -0700

    Need to complete product and customer testing

---
+++ Lab_6_Tests/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lab_6_Tests
+{
+    [TestFixture]
+    class ProductTests
+    {
+        [SetUp]
+        public void SetUpTests()
+        {
+
+        }
+
+        [Test]
+        public void Test
+    }
+}
