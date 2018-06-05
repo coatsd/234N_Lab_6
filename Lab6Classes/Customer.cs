c879eb1eb95e7c2f319059789a825984a719b59d
Daniel Coats <coatsd@my.lanecc.edu>
2018-06-05 15:35:07 -0700

    Completed Customer class. Might need to write tests

--- Lab6Classes/Customer.cs
+++ Lab6Classes/Customer.cs
@@ -4,9 +4,268 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ToolsCSharp;
+using Lab_6_DBClasses;
+using Lab_6_PropsClasses;
+
+// *** I had to change this
+using CustomerDB = Lab_6_DBClasses.CustomerDB;
+
+using System.Data;
+
 namespace Lab6Classes
 {
-    class Customer
+    public class Customer : BaseBusiness
     {
+        public override object GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetDefaultProperties()
+        {
+        }
+
+        /// <summary>
+        /// Sets required fields for a record.
+        /// </summary>
+        protected override void SetRequiredRules()
+        {
+            mRules.RuleBroken("Name", true);
+            mRules.RuleBroken("Address", true);
+            mRules.RuleBroken("ZipCode", true);
+        }
+
+        /// <summary>
+        /// Instantiates mProps and mOldProps as new Props objects.
+        /// Instantiates mbdReadable and mdbWriteable as new DB objects.
+        /// </summary>
+        protected override void SetUp()
+        {
+            mProps = new CustomerProps();
+            mOldProps = new CustomerProps();
+
+            if (this.mConnectionString == "")
+            {
+                mdbReadable = new CustomerDB();
+                mdbWriteable = new CustomerDB();
+            }
+
+            else
+            {
+                mdbReadable = new CustomerDB(this.mConnectionString);
+                mdbWriteable = new CustomerDB(this.mConnectionString);
+            }
+        }
+
+        #region constructors
+        /// <summary>
+        /// Default constructor - does nothing.
+        /// </summary>
+        public Customer() : base()
+        {
+        }
+
+        /// <summary>
+        /// One arg constructor.
+        /// Calls methods SetUp(), SetRequiredRules(), 
+        /// SetDefaultProperties() and BaseBusiness one arg constructor.
+        /// </summary>
+        /// <param name="cnString">DB connection string.
+        /// This value is passed to the one arg BaseBusiness constructor, 
+        /// which assigns the it to the protected member mConnectionString.</param>
+        public Customer(string cnString)
+            : base(cnString)
+        {
+        }
+
+        /// <summary>
+        /// Two arg constructor.
+        /// Calls methods SetUp() and Load().
+        /// </summary>
+        /// <param name="key">ID number of a record in the database.
+        /// Sent as an arg to Load() to set values of record to properties of an 
+        /// object.</param>
+        /// <param name="cnString">DB connection string.
+        /// This value is passed to the one arg BaseBusiness constructor, 
+        /// which assigns the it to the protected member mConnectionString.</param>
+        public Customer(int key, string cnString)
+            : base(key, cnString)
+        {
+        }
+
+        public Customer(int key)
+            : base(key)
+        {
+        }
+
+        // *** I added these 2 so that I could create a 
+        // business object from a properties object
+        // I added the new constructors to the base class
+        public Customer(CustomerProps props)
+            : base(props)
+        {
+        }
+
+        public Customer(CustomerProps props, string cnString)
+            : base(props, cnString)
+        {
+        }
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Read-only ID property.
+        /// </summary>
+        public int CustomerID
+        {
+            get
+            {
+                return ((CustomerProps)mProps).customerID;
+            }
+        }
+
+        /// <summary>
+        /// Read/Write property. 
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return ((CustomerProps)mProps).name;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).name))
+                {
+                    if (value.Trim().Length < 100 && value.Trim().Length > 0)
+                    {
+                        mRules.RuleBroken("Name", false);
+                        ((CustomerProps)mProps).name = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Name must be between 1 and 100 characters.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read/Write property. 
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// 
+        /// </exception>
+        public string Address
+        {
+            get
+            {
+                return ((CustomerProps)mProps).address;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).address))
+                {
+                    if (value.Trim().Length > 0 && value.Trim().Length < 50)
+                    {
+                        mRules.RuleBroken("Address", false);
+                        ((CustomerProps)mProps).address = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentException("Address must be between 1 and 50 characters");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read/Write property. 
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// 
+        /// </exception>
+        public string City
+        {
+            get
+            {
+                return ((CustomerProps)mProps).city;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).city))
+                {
+                    if (value.Length >= 1 && value.Length <= 20)
+                    {
+                        mRules.RuleBroken("City", false);
+                        ((CustomerProps)mProps).city = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentException("City must be between 1 and 20 characters");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read/Write property. 
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Thrown if the value is null or less than 1.
+        /// </exception>
+        public string State
+        {
+            get
+            {
+                return ((CustomerProps)mProps).state;
+            }
+
+            set
+            {
+                if (value.Length == 2)
+                {
+                    ((CustomerProps)mProps).state = value;
+                    mIsDirty = true;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Value must be 2 characters and represent's a state initial");
+                }
+            }
+        }
+
+        public string ZipCode
+        {
+            get
+            {
+                return ((CustomerProps)mProps).zipCode;
+            }
+            set
+            {
+                if (value.Trim().Length >= 5 && value.Trim().Length <= 15)
+                {
+                    ((CustomerProps)mProps).zipCode = value;
+                    mIsDirty = true;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("ZipCode needs to be between 5 and 15 characters");
+                }
+            }
+        }
+
+        #endregion Properties
     }
 }
